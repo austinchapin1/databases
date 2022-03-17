a1967e01e9ac466145670d0c1248f7b43543071a
Austin Chapin <austinchapin04@gmail.com>
2022-03-16 18:52:04 -0700

    Stubbed out and semi completed functions in messages.js, template from resources input in users.js

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,25 @@
 var db = require('../db');
 
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
-  create: function () {} // a function which can be used to insert a message into the database
+  // a function which produces all the messages
+  getAll: function (callback) {
+    connection.query('SELECT messages FROM messageList'), (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        callback(null, results);
+      }
+    };
+  },
+  // a function which can be used to insert a message into the database
+  create: function (callback) {
+    connection.query('INSERT INTO messageList (id, users, messages, rooms) VALUES (id, users, messages, rooms)'), (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        callback(null, results);
+      }
+    };
+  }
 };
+
